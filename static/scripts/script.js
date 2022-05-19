e664dc55a740b72cd31affef9b363963ea0ca181
spanifer <kovacsszabolcs91@gmail.com>
2022-05-19 17:40:59 +0100

    Add rent-intro to Home and toggle btn

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+  const sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  document.getElementById('rent-intro-toggle').addEventListener('click', toggleRentIntro);
+
+});
+
+function toggleRentIntro() {
+  let e = document.getElementById("rent-intro");
+  e.classList.toggle('hide')
+}

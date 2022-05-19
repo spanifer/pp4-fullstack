e358485ab6a3f03af65980368f6769f1d60f8a08
spanifer <kovacsszabolcs91@gmail.com>
2022-05-19 14:45:48 +0100

    Add Hero img (placeholder) and style Position the navbar and the hero content to honor design

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'hero_title': 'Find the best properties to rent in the area',
+    }
+    return render(request, 'index.html', context)

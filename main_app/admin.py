52dd7ccaa9166c3a6fa188efa60192536146b966
spanifer <kovacsszabolcs91@gmail.com>
2022-05-19 22:15:28 +0100

    Add Property model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Properties
 
-# Register your models here.
+admin.site.register(Properties)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Properties(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    address = models.CharField(max_length=200)
+    main_img = CloudinaryField('image')
+    list_date = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-list_date']
+
+    def __str__(self):
+        return self.name

8afbbb6736c2a68a1ebb141bdae548837836e855
spanifer <kovacsszabolcs91@gmail.com>
2022-05-19 23:07:15 +0100

    wip: Add Property-browser view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('property-browser/', views.PropertyBrowser.as_view(), name='properties'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Properties
 
 def home(request):
     context = {
         'hero_title': 'Find the best properties to rent in the area',
     }
     return render(request, 'index.html', context)
+
+
+class PropertyBrowser(View):
+
+    def get(self, request, *args, **kwargs):
+        properties = Properties.objects.filter(is_published=True)
+        context = {
+            'properties': properties,
+        }
+        return render(request, 'property-browser.html', context=context)

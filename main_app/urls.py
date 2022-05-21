9e7eacaddb83c254ea49bf6c8f525f4171acd0a3
spanifer <kovacsszabolcs91@gmail.com>
2022-05-21 21:35:24 +0100

    Add ViewRequest template and view, update urls WIP FormView is probably unsuitable for this use case.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('property-browser/', views.PropertyBrowser.as_view(), name='properties'),
+    path('properties/', views.PropertyBrowser.as_view(), name='properties'),
+    path('viewing/<int:propery_id>', views.Viewing.as_view(), name='viewing'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
-import imp
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
-from .models import Properties
+from django.views.generic import ListView, FormView
+from .models import Properties, ViewingRequest
+from .forms import ViewingRequestForm
 
 class Home(View):
     def get(self, request):
@@ -18,3 +18,29 @@ class PropertyBrowser(ListView):
     template_name = 'property-browser.html'
     context_object_name = 'properties'
     paginate_by = 3
+
+
+class Viewing(FormView):
+    model = ViewingRequest
+    form_class = ViewingRequestForm
+    template_name = 'viewing-request.html'
+    success_url = '/'
+
+    # Solution to include the property_id in the form context
+    # https://stackoverflow.com/questions/8903601/how-to-process-a-form-via-get-or-post-using-class-based-views
+    def get(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        context['property'] = Properties.objects.get(id=kwargs['propery_id'])
+        return self.render_to_response(context)
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print(form.errors)
+        return super().form_invalid(form)
+    

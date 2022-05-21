4c98accb7a8dcb2fe29d1324670d7a381f84e46a
spanifer <kovacsszabolcs91@gmail.com>
2022-05-21 21:30:42 +0100

    Add form for Viewing Request and register on admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Properties
+from .models import Properties, ViewingRequest
 
 admin.site.register(Properties)
+admin.site.register(ViewingRequest)
---
+++ main_app/forms.py
@@ -0,0 +1,9 @@
+from socket import fromshare
+from django import forms
+from .models import ViewingRequest
+
+
+class ViewingRequestForm(forms.ModelForm):
+    class Meta:
+        model = ViewingRequest
+        fields = ('fullname', 'phone', 'email', 'message')

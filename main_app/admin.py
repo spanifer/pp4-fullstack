e14c0b6632199c0b371edecba26f749b8fc871c3
spanifer <kovacsszabolcs91@gmail.com>
2022-05-26 13:54:22 +0100

    Add UserProfile model and register on admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Properties, ViewingRequest
+from .models import Properties, ViewingRequest, UserProfile
 
+admin.site.register(UserProfile)
 admin.site.register(Properties)
 admin.site.register(ViewingRequest)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=14, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Properties(models.Model):
     name = models.CharField(max_length=100, unique=True)

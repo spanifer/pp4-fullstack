2d520a4bcca094638b7b399e63e3bed9d726f612
spanifer <kovacsszabolcs91@gmail.com>
2022-05-26 13:57:08 +0100

    Add Login, Register, and UserProfile forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,28 @@
 from django import forms
-from .models import ViewingRequest
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import ViewingRequest, UserProfile
 
 
 class ViewingRequestForm(forms.ModelForm):
     class Meta:
         model = ViewingRequest
         fields = ('fullname', 'phone', 'email', 'message')
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'email', 'first_name', 'last_name']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['phone']

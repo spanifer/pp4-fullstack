b9399592ad3bcc07ce909de8ca28424e8c9e56da
spanifer <kovacsszabolcs91@gmail.com>
2022-05-27 16:24:22 +0000

    Add double booking check for ViewingRequest and only allow user to book a viewing if he is logged in Add message for successful booking Associate User model with the ViewingRequest

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import ListView
-from .models import Properties
+from django.contrib import messages
+from .models import Properties, ViewingRequest
 from .forms import ViewingRequestForm
 
 
@@ -22,17 +24,33 @@ class PropertyBrowser(ListView):
 
 
 class Viewing(View):
-    success_url = '/'
     template_name = 'viewing-request.html'
+    success_message = (
+        'We received your request, we will get back to you shortly.')
+    permission_denied_message = (
+        'You need to log in to your account to book a viewing')
+    already_booked_message = (
+        'You have previously requested a viewing for this property')
 
     def get(self, request, property_id, *args, **kwargs):
-        form = ViewingRequestForm()
+        user = request.user
+        # Check if user is logged in
+        if not user.is_authenticated:
+            messages.warning(request, self.permission_denied_message)
+            return redirect('login')
+
+        # Check if user already booked an appointment for this property
+        user_viewings = ViewingRequest.objects.filter(for_user=user)
         _property = Properties.objects.get(id=property_id)
+        if user_viewings.filter(property=_property).count() > 0:
+            messages.warning(request, self.already_booked_message)
+            return redirect('home')
+
+        form = ViewingRequestForm()
         context = {
             'property': _property,
             'form': form,
         }
-
         return render(request, self.template_name, context)
 
     def post(self, request, property_id, *args, **kwargs):
@@ -43,5 +61,8 @@ class Viewing(View):
         if form.is_valid():
             obj = form.save(commit=False)
             obj.property = Properties.objects.get(id=property_id)
+            obj.for_user = request.user
             obj.save()
-            return render(request, self.success_url)
+
+            messages.success(request, self.success_message)
+            return redirect('home')

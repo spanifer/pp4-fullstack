a99ac57a5f568b493f41c306da24d7bbb46e0123
spanifer <kovacsszabolcs91@gmail.com>
2022-05-30 15:49:44 +0000

    Add customized Properties and ViewingRequest admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,8 +3,27 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Properties, ViewingRequest, UserProfile
 
-admin.site.register(Properties)
-admin.site.register(ViewingRequest)
+
+@admin.register(Properties)
+class PropertiesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available', 'is_published')
+    list_filter = ('is_published', 'available', 'list_date')
+    actions = ['publish', 'toggle_availability']
+
+    def publish(self, request, queryset):
+        queryset.update(is_published=True)
+
+    def toggle_availability(self, request, queryset):
+        queryset.update(available=not queryset[0].available)
+
+
+@admin.register(ViewingRequest)
+class ViewingRequestAdmin(admin.ModelAdmin):
+    list_filter = ('request_date', 'arranged_date', 'for_user')
+    search_fields = (
+        'for_user__email', 'for_user__first_name',
+        'for_user__last_name')
+    readonly_fields = ('property', 'message', 'request_date', 'for_user')
 
 
 # Solution from https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
